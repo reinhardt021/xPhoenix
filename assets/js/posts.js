87d9d9bd0e53f31af9e2fb535d1028f0df80c77f
Reinhardt Cagara <reinhardt.cgr@gmail.com>
2018-08-12 23:50:14 -0700

    Added some notes for next session

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -6,7 +6,6 @@
 
     var boardID = 'post-board';
 
-
     document.getElementById(boardID).addEventListener('click', function (event) {
         var newPost = document.createElement('div');
         var postText = document.createElement('p');
@@ -18,7 +17,7 @@
         // https://stackoverflow.com/questions/7790725/javascript-track-mouse-position
         var positionX = event.pageX;
         var positionY = event.pageY;
-        
+
         newPost.style.position = 'absolute';
         newPost.style.left = positionX + 'px';
         newPost.style.top = positionY + 'px';
@@ -28,6 +27,12 @@
         // new div will be default size: 130px by 80px to keep golden ratio
 
         // TODO: click and drag to create text boxes of a specific size and location
+
+        // TODO: click to edit postTex
+        // TODO: center text with Flexbox / Bulma
+        // TODO: create text toolbar to edit the text in the boxes
+        // bold + italix + H1 H2 H3 H4 P + bullet point + 
+        // TODO: Create sidebar with diff headers
     }, false);
 
 })();
