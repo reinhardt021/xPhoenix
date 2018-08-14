ab6d64f60fa1ed48522bd872d4a4defa4ec5d198
Reinhardt Cagara <reinhardt.cgr@gmail.com>
2018-08-13 22:25:01 -0700

    refactored the methods to be a little more readable

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,76 +1,113 @@
 (function () {
     // TODO: ajax call for GET /api/posts
 
+    // TODO: create text toolbar to edit the text in the boxes
+    // bold + italix + H1 H2 H3 H4 P + bullet point +
+    // TODO: Create sidebar with diff headers
+
     var boardID = 'post-board';
     var theBoard = document.getElementById(boardID);
 
     function _createDeleteButton() {
-        var deletePost = document.createElement('a');
+        var deleteButton = document.createElement('a');
         var deleteClasses = [
             'delete',
             'is-pulled-right'
         ];
-        deletePost.setAttribute('class', deleteClasses.join(' '));
+        deleteButton.setAttribute('class', deleteClasses.join(' '));
 
-        return deletePost;
+        return deleteButton;
+    }
+
+    function _createDefaultPostContent() {
+        var postContent = document.createElement('p');
+        var postText = document.createTextNode('Edit Me!');
+        postContent.appendChild(postText);
+
+        return postContent;
+    }
+
+    function _createBasicPost() {
+        var newPost = document.createElement('div');
+        newPost.appendChild(_createDeleteButton());
+        newPost.appendChild(_createDefaultPostContent());
+
+        var postClasses = ['a-post', 'box'];
+        newPost.setAttribute('class', postClasses.join(' '));
+        // newPost.setAttribute('contenteditable', true); //TODO: turn on later
+        // TODO: unfocus to save Post text
+
+        return newPost;
+    }
+
+    function _addPostEvents(newPost) {
+        newPost.addEventListener('mousedown', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            console.log('>>> post mousedown');
+
+            newPost.onmousemove = function(e) {
+                e.preventDefault();
+                console.log('>>> post moving');
+                newPost.style.left = e.pageX;
+                newPost.style.top = e.pageY;
+                //NOTE: this doesn't work unless you are above the post
+            };
+
+            newPost.onmouseup = function(e) {
+                e.stopPropagation();
+                newPost.onmousemove = null;
+                console.log('>>> post mouse up');
+                //NOTE: this doesn't work unless you are above the post
+            };
+        }, false);
+
+        return newPost;
     }
 
     theBoard.addEventListener('mousedown', function (e) {
         e.preventDefault();
-        console.log('>>> mousedown');
+        console.log('>>> board mousedown');
 
-        var newPost = document.createElement('div');
-
-        newPost.appendChild(_createDeleteButton());
-
-        var postText = document.createElement('p');
-        var postContent = document.createTextNode('Edit Me!');
-        postText.appendChild(postContent);
-
-        newPost.appendChild(postText);
-        var postClasses = [
-            'a-post',
-            'box'
-        ];
-        newPost.setAttribute('class', postClasses.join(' '));
-        // newPost.setAttribute('contenteditable', true); //TODO: turn on later
+        var newPost = _createBasicPost();
 
         // https://stackoverflow.com/questions/7790725/javascript-track-mouse-position
-        var positionX = e.pageX;
-        var positionY = e.pageY;
-
+        var postOriginX = e.pageX;
+        var postOriginY = e.pageY;
         newPost.style.position = 'absolute';
-        newPost.style.left = positionX + 'px';
-        newPost.style.top = positionY + 'px';
+        newPost.style.left = postOriginX + 'px';
+        newPost.style.top = postOriginY + 'px';
+
         var height = 80;
         var width = 130;
         newPost.style.height = height + 'px';
         newPost.style.width = width + 'px';
-
-        newPost.addEventListener('mousedown', function (e) {
-            console.log('>>> clicked on a post and will stop propagation');
-            e.stopPropagation();
-        }, false);
-
-        theBoard.appendChild(newPost);
-        // new div will be default size: 130px by 80px to keep golden ratio
-
         // TODO: click and drag to create text boxes of a specific size and location
 
-        // TODO: click to edit postTex
-        // TODO: center text with Flexbox / Bulma
-        // TODO: create text toolbar to edit the text in the boxes
-        // bold + italix + H1 H2 H3 H4 P + bullet point +
-        // TODO: Create sidebar with diff headers
+        // Mouse down event for Post
+        _addPostEvents(newPost)
+
+        theBoard.appendChild(newPost);
 
         theBoard.onmousemove = function(e) {
-            console.log('>>> X', e.pageX);
-            console.log('>>> Y', e.pageY);
+            e.preventDefault();
+            console.log('>>> board moving');
+
+            // TODO: delete and create new post in order to get proper dimensions
+            
+            var postWidth = e.pageX - postOriginX;
+            var postHeight = e.pageY - postOriginY;
+            console.log('>>> board X = ' + e.pageX + ' >> width = ' + postWidth);
+            console.log('>>> board Y = ' + e.pageY + ' >> height = ' + postHeight);
+            newPost.style.width = postWidth;
+            newPost.style.height = postHeight;
+            theBoard.appendChild(newPost);
         };
 
         theBoard.onmouseup = function() {
+            e.stopPropagation();
             theBoard.onmousemove = null;
-            console.log('>>> mouseup 02');
+            console.log('>>> board mouse up');
         };
     }, false);
 
