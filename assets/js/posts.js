4f3a261a1e664c3b5f1c745f569f1a56cb6bcf67
Reinhardt Cagara <reinhardt.cgr@gmail.com>
2018-08-13 20:47:46 -0700

    added the event listener on the post to prevent propogation

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,12 +1,10 @@
 (function () {
-    // need Jquery first??
-    console.log('Testing one two three');
-
     // TODO: ajax call for GET /api/posts
 
     var boardID = 'post-board';
+    var theBoard = document.getElementById(boardID);
 
-    document.getElementById(boardID).addEventListener('click', function (event) {
+    theBoard.addEventListener('click', function (event) {
         var newPost = document.createElement('div');
         var postText = document.createElement('p');
         var postContent = document.createTextNode('Edit Me!');
@@ -22,8 +20,12 @@
         newPost.style.left = positionX + 'px';
         newPost.style.top = positionY + 'px';
 
-        var board = document.getElementById(boardID);
-        board.appendChild(newPost);
+        newPost.addEventListener('click', function (event) {
+            console.log('>>> clicked on a post and will stop propagation');
+            event.stopPropagation();
+        }, false);
+
+        theBoard.appendChild(newPost);
         // new div will be default size: 130px by 80px to keep golden ratio
 
         // TODO: click and drag to create text boxes of a specific size and location
@@ -31,7 +33,7 @@
         // TODO: click to edit postTex
         // TODO: center text with Flexbox / Bulma
         // TODO: create text toolbar to edit the text in the boxes
-        // bold + italix + H1 H2 H3 H4 P + bullet point + 
+        // bold + italix + H1 H2 H3 H4 P + bullet point +
         // TODO: Create sidebar with diff headers
     }, false);
 
